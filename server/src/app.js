43aab9138cb185d34359085c7ca9b19c29a5c941
akshaykh123 <akshay.kh123@gmail.com>
2026-06-08 17:18:55 +0530

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Form Builder API Running",
+  });
+});
+
+export default app;
